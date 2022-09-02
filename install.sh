679976acdde6ea0f21a4eedadf808e98dd4907bb
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2022-09-02 16:43:43 -0400

    fix installation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 set -e
 
 cp -r . $HOME
@@ -8,20 +8,17 @@ if [[ ! -a $HOME/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-. $HOME/.zshrc
-
-
 if [[ ! -a $HOME/.fzf ]]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     $HOME/.fzf/install --all
 fi
 
 if [[ ! -a $HOME/.go ]]; then
-    curl -L https://git.io/vQhTU | GOROOT=$HOME/.go sh
+    curl -L https://git.io/vQhTU | GOROOT=$HOME/.go bash
 fi
 
 if [[ ! -a $HOME/.nvm ]]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | NVM_DIR=$HOME/.nvm zsh
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | NVM_DIR=$HOME/.nvm bash
 fi
 
 if [[ ! -a $GOPATH/bin/powerline-go ]]; then
